a2f5af83cb15f1929b32ce56528cc8b8eb76ac4b
akashmandal099 <akashmandal099@gmail.com>
2022-05-26 01:47:55 +0530

    Added API-Gateway

---
+++ Api-Gateway/src/main/java/com/akash/ApiGateway/gatewayConfig.java
@@ -0,0 +1,16 @@
+package com.akash.ApiGateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class gatewayConfig {
+	
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+		return builder.routes().build();
+	}
+
+}
